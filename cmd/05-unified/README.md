75658205ad67af285275db3a83af59de578c219a
Lukas Schwab <lukas.schwab@gmail.com>
2022-10-09 16:48:06 -0700

    Analyze counterintuitive graph, add superlinear weights

--- cmd/05-unified/README.md
+++ cmd/05-unified/README.md
@@ -1,3 +1,17 @@
 # 05-unified
 
 Attempt at producing a lot of comparisons all at once, using learnings from the previous experiments.
+
+## Interpretation
+
+The `clever-degreePositive-tricky` graph was *very* unintuitive to me at first, but I think I've figured it out!
+
+I expected this to be the *most* punishing graph for the clever algorithm... but the clever algorithm performs almost optimally, for all $n, k$! What gives?
+
+First important realization: the theoretical lightest vertex cover with `DegreePositive` weights has weight $\|E\|$ by definition of the vertex cover! So all a "good" strategy can do in this context is *minimize redundancy.*
+
+Vazirani is inherently redundant.
+
+If I introduce a superlinear degree-correlated weight scheme, I can't imagine clever will perform well.
+
+This is a great counterintuitive explanation.
--- pkg/graph/weights.go
+++ pkg/graph/weights.go
@@ -6,10 +6,18 @@ import (
 
 // Named weighers.
 var (
-	Uniform        Weigher = uniform{}
-	Random                 = random{}
-	DegreeNegative         = degreeNegative{}
-	DegreePositive         = degreePositive{}
+	// Uniform Weigher. Every vertex gets weight 1.
+	Uniform Weigher = uniform{}
+	// Random Weigher. Every vertex gets a pseudorandom weight on [0, 1).
+	Random = random{}
+	// DegreeNegative gives each vertex a weight inversely proportional to its
+	// degree.
+	DegreeNegative = degreeNegative{}
+	// DegreePositive gives each vertex a weight equal to its degree.
+	DegreePositive = degreePositive{}
+	// DegreePositiveSuperlinear gives each vertex a weight equal to the square
+	// of its degree.
+	DegreePositiveSuperlinear = degreePositiveSuperlinear{}
 )
 
 // Weigher for vertices in a [graph.Weighted].
@@ -17,31 +25,32 @@ type Weigher interface {
 	weigh(Vertex, *Neighbors) float32
 }
 
-// Uniform Weigher. Every vertex gets weight 1.
 type uniform struct{}
 
 func (u uniform) weigh(v Vertex, ns *Neighbors) float32 {
 	return float32(1)
 }
 
-// Random Weigher. Every vertex gets a pseudorandom weight on [0, 1).
 type random struct{}
 
 func (r random) weigh(v Vertex, ns *Neighbors) float32 {
 	return rand.Float32()
 }
 
-// DegreeNegative Weigher gives each vertex a weight inversely proportional to
-// its degree.
 type degreeNegative struct{}
 
 func (d degreeNegative) weigh(v Vertex, ns *Neighbors) float32 {
 	return 1.0 / (float32(ns.Length()) - 0.0001)
 }
 
-// DegreePositive gives each vertex a weight equal to its degree.
 type degreePositive struct{}
 
 func (d degreePositive) weigh(v Vertex, ns *Neighbors) float32 {
 	return float32(ns.Length())
+}
+
+type degreePositiveSuperlinear struct{}
+
+func (d degreePositiveSuperlinear) weigh(v Vertex, ns *Neighbors) float32 {
+	return float32(ns.Length() * ns.Length())
 }
